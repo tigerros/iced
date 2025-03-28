0e818ba32132c8a985d3a28c285a57f8c02602dc
tigerros <aurel.leonard.danel@gmail.com>
2025-03-28 20:07:58 +0100

    scrap the responsive, just make it a function on `Size<f32>`

--- examples/aspect_ratio/src/main.rs
+++ examples/aspect_ratio/src/main.rs
@@ -1,5 +1,5 @@
-use iced::widget::{aspect_ratio, center, column, container};
-use iced::{Background, Color, Element};
+use iced::widget::{button, center, column, container, responsive, row};
+use iced::{Background, Color, Element, Length, Size};
 
 fn main() {
     iced::run("aspect ratio", AspectRatio::update, AspectRatio::view).unwrap();
@@ -14,41 +14,59 @@ impl AspectRatio {
     fn view(&self) -> Element<()> {
         center(
             column![
-                aspect_ratio(
-                    1.0,
-                    || container("Green 1:1 square").style(|_| container::Style {
+                responsive(move |size| {
+                    let row_height = 40.0;
+                    let square_available =
+                        Size::new(size.width, size.height - row_height);
+                    let square_size = square_available.aspect_ratio(1.0);
+
+                    column![
+                        container("Green 1:1 square with buttons")
+                            .style(|_| container::Style {
+                                text_color: None,
+                                background: Some(Background::Color(
+                                    Color::from_rgb8(0, 100, 0)
+                                )),
+                                border: Default::default(),
+                                shadow: Default::default(),
+                            })
+                            .width(square_size.width)
+                            .height(square_size.height),
+                        row![
+                            button("<").width(Length::Fill),
+                            button(">").width(Length::Fill),
+                        ]
+                        .width(square_size.width)
+                        .height(row_height)
+                    ]
+                    .into()
+                }),
+                responsive(move |size| {
+                    let ratioed = size.aspect_ratio(16.0 / 9.0);
+
+                    center(
+                        container("Blue 16:9 container")
+                            .style(|_| container::Style {
+                                text_color: None,
+                                background: Some(Background::Color(
+                                    Color::from_rgb8(0, 0, 100),
+                                )),
+                                border: Default::default(),
+                                shadow: Default::default(),
+                            })
+                            .width(ratioed.width)
+                            .height(ratioed.height),
+                    )
+                    .style(|_| container::Style {
                         text_color: None,
                         background: Some(Background::Color(Color::from_rgb8(
-                            0, 100, 0
+                            0, 0, 80,
                         ))),
                         border: Default::default(),
                         shadow: Default::default(),
-                    }),
-                    |ratioed| center(ratioed)
-                        .style(|_| container::Style {
-                            text_color: None,
-                            background: Some(Background::Color(
-                                Color::from_rgb8(100, 0, 0)
-                            )),
-                            border: Default::default(),
-                            shadow: Default::default(),
-                        })
-                        .into()
-                ),
-                aspect_ratio(
-                    16.0 / 9.0,
-                    || container("Blue 16:9 container").style(|_| {
-                        container::Style {
-                            text_color: None,
-                            background: Some(Background::Color(
-                                Color::from_rgb8(0, 0, 100),
-                            )),
-                            border: Default::default(),
-                            shadow: Default::default(),
-                        }
-                    }),
-                    |ratioed| center(ratioed).into()
-                ),
+                    })
+                    .into()
+                }),
             ]
             .spacing(20),
         )
